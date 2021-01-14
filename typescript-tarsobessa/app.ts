2fb407e6603c3038f2ed0a5589a7338a8ccf363a
Alessandro Lima <alessandro.lima@msn.com>
2021-01-14 17:09:47 -0300

    Revisando Typescript

---
+++ typescript-tarsobessa/app.ts
@@ -0,0 +1,14 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!"
+console.log(message);
+
+let episode: number = 4;
+console.log("This is episode "+ 4);
+episode = episode + 1;
+console.log("Next episode is "+ episode);
+
+// let favoriteDroid; // Se não especificar o tipo, fica any
+let favoriteDroid: string;
+favoriteDroid = 'BB-8';
+console.log("My favorite droid is "+ favoriteDroid);
+
+
---
+++ typescript-tarsobessa/dist/js/app.js
@@ -0,0 +1,10 @@
+var message = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(message);
+var episode = 4;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+// let favoriteDroid; // Se não especificar o tipo, fica any
+var favoriteDroid;
+favoriteDroid = 'BB-8';
+console.log("My favorite droid is " + favoriteDroid);
