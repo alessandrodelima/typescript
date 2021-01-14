70a853d62035231a5758b36276af2553f385437f
Alessandro Lima <alessandro.lima@msn.com>
2021-01-14 18:05:02 -0300

    Revisando Variáveis, Funções em TypeScript

--- typescript-tarsobessa/dist/js/app.js
+++ typescript-tarsobessa/dist/js/app.js
@@ -8,3 +8,30 @@ console.log("Next episode is " + episode);
 var favoriteDroid;
 favoriteDroid = 'BB-8';
 console.log("My favorite droid is " + favoriteDroid);
+//Functions 
+var shortestRun = function (parsecs) {
+    return parsecs < 12;
+};
+console.log(shortestRun(9));
+function inc(speed, inc) {
+    return speed + inc;
+}
+console.log(inc(5, 1)); // 6
+//Parâmetros REST 
+function countJedis(jedis) {
+    return jedis.reduce(function (a, b) { return a + b; }, 0);
+}
+console.log(countJedis([2, 3, 4])); // 9
+var isEnoughtToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 11;
+console.log("Is " + distance + " parsecs enough to beat Millennium Falcon? " + (isEnoughtToBeatMF(distance) ? 'YES' : 'NO'));
+var call = function (name) { return console.log("Do you copy, " + name + "?"); };
+call('R2');
+function inc2(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5,1) = " + inc2(5, 1));
+console.log("inc (5) = " + inc2(5));
